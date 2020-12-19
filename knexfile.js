5163b9588b4fdc8b4c10b864c8db7c01faf1cc5f
“Quinn <quinnlashinsky@gmail.com>
2020-12-18 21:43:09 -0500

    Replace values in knex development and test connection object with env variables

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,10 @@ module.exports = {
     client: "pg",
     version: "12",
     connection: {
-      host: "127.0.0.1",
-      user: "quinnlashinsky",
-      password: "",
-      database: "quizletdev",
+      host: process.env.DATABASE_HOST || "127.0.0.1",
+      user: process.env.DEVELOPMENT_DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DEVELOPMENT_DATABASE_NAME,
     },
     migrations: {
       directory: __dirname + "/knex/migrations",
@@ -21,10 +21,10 @@ module.exports = {
     client: "pg",
     version: "12",
     connection: {
-      host: "127.0.0.1",
-      user: "quinnlashinsky",
-      password: "",
-      database: "quizletdevtest",
+      host: process.env.TEST_DATABASE_HOST || "127.0.0.1",
+      user: process.env.TEST_DATABASE_USER,
+      password: process.env.TEST_DATABASE_PASSWORD,
+      database: process.env.TEST_DATABASE_NAME,
     },
     migrations: {
       directory: __dirname + "/knex/migrations",
