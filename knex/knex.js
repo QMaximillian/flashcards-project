212ef71c31dbb841656071ac97dde83bfd612a15
“Quinn <quinnlashinsky@gmail.com>
2021-04-06 19:50:46 -0400

    Adjust ssl

--- knex/knex.js
+++ knex/knex.js
@@ -1,3 +1,5 @@
+let pg = require("pg");
+pg.defaults.ssl = true;
 const environment = process.env.NODE_ENV || "development";
 const config = require("../knexfile.js")[environment];
 module.exports = require("knex")(config);
