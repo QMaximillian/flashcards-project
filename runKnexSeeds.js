43b0506433e939316b78a397ab79f2e543ba3f99
“Quinn <quinnlashinsky@gmail.com>
2021-04-07 12:44:14 -0400

    Remove space in runKnexSeeds.js

--- runKnexSeeds.js
+++ runKnexSeeds.js
@@ -5,7 +5,7 @@ exec(
   "knex migrate:rollback && knex migrate:latest && knex seed:run --specific=01_users.js && knex seed:run --specific=02_card_sets.js && knex seed:run --specific=03_flashcards.js && knex seed:run --specific=04_users_card_sets.js",
   (error, stdout, stderr) => {
     if (error) {
-      console.log(`error: ${error.message} `);
+      console.log(`error: ${error.message}`);
       return;
     }
     if (stderr) {
